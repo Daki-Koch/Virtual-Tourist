0a997e9c13e982bd94540e76209551bfeb3e58d4
Daki-Koch <57409868+Daki-Koch@users.noreply.github.com>
2022-12-17 11:51:05 +0100

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by David Koch on 17.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
